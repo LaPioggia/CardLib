831d911393524e6f212a46d20815c0b9cf34179b
LaPioggia <jsdt016352@gmail.com>
2021-10-03 09:43:05 +0800

    cardlib创建

---
+++ CardLib/Rank.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardLib
+{
+	/// <summary>
+	/// rank
+	/// </summary>
+	public enum Rank
+	{
+		Ace = 1,
+		Deuce,
+		Three,
+		Four,
+		Five,
+		Six,
+		Seven,
+		Eight,
+		Nine,
+		Ten,
+		Jack,
+		Queen,
+		King
+	}
+}
---
+++ CardLib/Suit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardLib
+{
+	/// <summary>
+	/// suit
+	/// </summary>
+	public enum Suit
+	{
+		Club,
+		Diamond,
+		Heart,
+		Spade
+	}
+}
